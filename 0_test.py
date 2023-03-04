41c0a985460d82bfd32bc96a3eca3b6258886d43
LATITUDE <skostecki1@gmail.com>
2023-03-04 09:09:29 +0100

    Tester_04-05.03.2023

---
+++ 0_test.py
@@ -0,0 +1,9 @@
+napis = 'Kamil ma psa'
+lista_zwierzat = ['pies', 'drugi pies', 'tu tez pies']
+
+print(napis[2:6])
+print(lista_zwierzat[2])
+print(lista_zwierzat[2][3:7])
+
+for i in range(20, 4, -3):
+    print(i)
