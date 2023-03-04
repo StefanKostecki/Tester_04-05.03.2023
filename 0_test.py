d79555bd3bcb556b9e7fdae9b8f63386604d733a
LATITUDE <skostecki1@gmail.com>
2023-03-04 09:20:10 +0100

    Tester_04-05.03.2023

--- 0_test.py
+++ 0_test.py
@@ -5,5 +5,8 @@ print(napis[2:6])
 print(lista_zwierzat[2])
 print(lista_zwierzat[2][3:7])
 
-for i in range(20, 4, -3):
+for i in range(20, 4, -3):      #od, do, co ile (krok)
     print(i)
+
+for i in range(4):                # i jest iteratorem, możemy go użyć tak jak chcemy
+    print('okrazenie ',i+1, 'a litera nr ',i+4, 'to' 'napis[i+3]')
