d661ac370e72ce0af6f3f3c0a6e3d765e59f6aa2
LATITUDE <skostecki1@gmail.com>
2023-03-04 14:49:47 +0100

    Tester_04-05.03.2023

---
+++ selenium_OOP2.py
@@ -0,0 +1,12 @@
+import time
+
+from selenium import webdriver
+from selenium_OOP import LoginPage
+
+driver = webdriver.Chrome()
+page = LoginPage(driver)
+page.open()
+page.enter_username('standard_user')
+page.enter_password('secret_sauce')
+page.click_login()
+time.sleep(2)
