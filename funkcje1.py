1c9d6be11e7fac38e6e76b15ccbb9092df32ff5f
LATITUDE <skostecki1@gmail.com>
2023-03-04 12:28:44 +0100

    Tester_04-05.03.2023

---
+++ funkcje1.py
@@ -0,0 +1,16 @@
+from funkcje2_kod import *
+# imie - 'Kasia'
+# print(len(imie))
+# number = 2332.434
+# print(round(number, 3))
+
+welcome_basic()
+imie1 = 'Marysia'
+lista_imion = ['Adam', 'Marek']
+
+welcome_full('Waldemar', 38)
+welcome_full(imie1, 12)
+welcome_full(lista_imion[1], 94)
+
+print(stan_zdrowia(90, 1.70))
+print(stan_zdrowia(90))
